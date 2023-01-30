0a19017e7e1ea7e00a663da22852656e88ab6744
Ruaaa1983 <michaeliu1983@hotmail.com>
2023-01-30 22:10:28 +0800

    Site updated: 2023-01-30 22:10:28

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2020/01/01 00:00:00");
+  var start = new Date("2023/01/28 00:00:00");
 
   function update() {
     var now = new Date();
